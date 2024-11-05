55cacb7a9c2923b9f0287351651078c5bdea3fc3
dali <dalibor381@gmail.com>
2024-11-05 16:10:40 -0600

    disabling csrf for testing

--- coding-challenge/src/main/java/com/ama/agencybooks/config/SecurityConfig.java
+++ coding-challenge/src/main/java/com/ama/agencybooks/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -32,6 +33,8 @@ public class SecurityConfig  {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                //TODO disabling csrf for POST, PUT, must be enabled in production
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/public/**")
                         .permitAll())
